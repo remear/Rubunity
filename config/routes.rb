07d47930b46d609df45b989d1d1cda575b4726ed
Ben Mills <ben@unfiniti.com>
2011-02-16 22:21:45 -0700

    Added topic field and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rubunity::Application.routes.draw do
   resources :comments
 
   resources :bookmarks do
+    
     resources :comments
   end
-
-  root :to => "pages#home"
+  match "add_topic" => "bookmarks#add_topic", :as => "bookmark_add_topic"
 
   resources :users, :user_sessions
- 
+
   match "login" => "user_sessions#new", :as => "login"
   match "logout" => "user_sessions#destroy", :as => "logout"
   match "register" => "users#new", :as => "register"
@@ -20,4 +20,6 @@ Rubunity::Application.routes.draw do
   
   #pages
   match "botusage" => "pages#irc_bot_usage", :as => "bot_usage"
+  
+  root :to => "pages#home"
 end
